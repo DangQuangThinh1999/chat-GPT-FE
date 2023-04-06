6235993d9c02f77e84698da4f36bb378e95366b0
DangQuangThinh1999 <quangthinh123qb@gmail.com>
2023-04-06 22:25:39 +0700

    change url BE

--- src/pages/api/index.tsx
+++ src/pages/api/index.tsx
@@ -6,15 +6,18 @@ export default async function handler(
 ) {
   const { message } = req.body;
 
-  const response = await fetch("http://localhost:3080", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({
-      message: message,
-    }),
-  });
+  const response = await fetch(
+    "https://vercel.com/dangquangthinh1999/chat-gpt-be",
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        message: message,
+      }),
+    }
+  );
 
   const data = await response.json();
   res.status(200).json({
